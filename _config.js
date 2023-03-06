c48e8b09efebd05f05b8d9c88f6b634e3ce45667
ekimutai <enockkimutai95@gmail.com>
2023-03-06 12:04:57 +0300

    added a space on jenkins filed

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: ' mongodb+srv://enockngetich:@K1muta1h@cluster0.fciaitw.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://enockngetich:@K1muta1h@cluster0.fciaitw.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://enockngetich:@K1muta1h@cluster0.fciaitw.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
